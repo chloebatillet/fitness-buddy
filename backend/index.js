b0dfd852e120f4af26d571fe40e21f670f19e36b
chloebatillet <cbatillet@gmail.com>
2023-11-27 17:18:14 +0100

    vercel.json file

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,9 @@ require("dotenv").config();
 const router = require("./routers/router");
 //const sequelizeConnection = require("./models/connection");
 
-// Initialisation du serveur express
 const app = express();
 const port = process.env.PORT;
 
-// URL autorisées à consulter l'API
 app.use(
   cors({
     origin: "*",
@@ -19,10 +17,8 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Déclaration de l'utilsation du router
 app.use(router);
 
-// Lancement de l'écoute sur le port défini
 app.listen(port, () => {
   // console.log(`Example app listening on port ${port}`);
 });
